f06e9a2dfe013b9254980100e880254169c4c5fe
Mehdi HAFID <mehdi.hafid@outlook.com>
2024-01-14 15:18:52 +0000

    fully working secured backend, public key access through url

---
+++ src/main/java/com/derbyware/nidam/NidamApplication.java
@@ -0,0 +1,13 @@
+package com.derbyware.nidam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NidamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NidamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/derbyware/nidam/NidamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derbyware.nidam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NidamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
