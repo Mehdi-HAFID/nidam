14051509fcaa31d2a5f4f99b7943e0e5da9e4bad
Mehdi HAFID <mehdi.hafid@outlook.com>
2024-01-18 19:38:08 +0000

    support for the custom value added by token-generator with custom Authentication class with converter. /demo return authentication object.

--- src/main/java/com/derbyware/nidam/controller/DemoController.java
+++ src/main/java/com/derbyware/nidam/controller/DemoController.java
@@ -1,13 +1,33 @@
 package com.derbyware.nidam.controller;
 
+import com.derbyware.nidam.authentication.CustomAuthentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.DefaultOAuth2AuthenticatedPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 @RestController
 public class DemoController {
+	private Logger log = Logger.getLogger(DemoController.class.getName());
 
 	@GetMapping("/demo")
-	public String demo() {
-		return "Demo";
+	public Authentication demo(Authentication a) {
+//		log.info("a.getAuthorities(): " + a.getAuthorities());
+//		log.info("((Jwt) a.getPrincipal()).getClaims(): " + ((Jwt) a.getPrincipal()).getClaims());
+		log.info("((Jwt) a.getPrincipal()).getClaim('authorities'): " + ((Jwt) a.getPrincipal()).getClaim("authorities"));
+//		log.info("a.getCredentials(): " + a.getCredentials());
+//		log.info("a.getDetails(): " + a.getDetails());
+
+		CustomAuthentication cusAuth = (CustomAuthentication) a;
+		log.info("cusAuth.getAuthorities2(): " + cusAuth.getAuthorities2());
+//		log.info("cusAuth.getAuthorities(): " + cusAuth.getAuthorities().stream().map(ab -> ab.getAuthority()).toList());
+		return a;
 	}
 }
