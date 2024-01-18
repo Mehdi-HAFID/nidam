45948a5272bcec3c9ef97c45e2330f2395865c87
Mehdi HAFID <mehdi.hafid@outlook.com>
2024-01-18 19:38:08 +0000

    support for the custom value added by token-generator with custom Authentication class with converter. /demo return authentication object.

--- nidam/src/main/java/com/derbyware/nidam/ProjectConfig.java
+++ nidam/src/main/java/com/derbyware/nidam/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.derbyware.nidam;
 
+import com.derbyware.nidam.authentication.JwtAuthenticationConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +13,18 @@ public class ProjectConfig {
 	@Value("${keySetURI}")
 	private String keySetUri;
 
+	private final JwtAuthenticationConverter converter;
+
+	public ProjectConfig(JwtAuthenticationConverter converter) {
+		this.converter = converter;
+	}
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.oauth2ResourceServer(
 				c -> c.jwt(
 						j -> j.jwkSetUri(keySetUri)
+								.jwtAuthenticationConverter(converter)
 				)
 		);
 		http.authorizeHttpRequests(
