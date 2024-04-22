96a8b38a6c055a2966cef61b63ab874104c8721f
Mehdi HAFID <mehdi.hafid@outlook.com>
2024-04-22 16:22:45 +0000

    package rename

--- bff/src/main/java/nidam/bff/BffApplication.java
+++ bff/src/main/java/nidam/bff/BffApplication.java
@@ -1,4 +1,4 @@
-package bff.bff;
+package nidam.bff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- bff/src/test/java/nidam/bff/BffApplicationTests.java
+++ bff/src/test/java/nidam/bff/BffApplicationTests.java
@@ -1,4 +1,4 @@
-package bff.bff;
+package nidam.bff;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
